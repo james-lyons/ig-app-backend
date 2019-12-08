f86eeeeffcdafa061de207a4beb902d13f6627ba
pouyarezvani <32917154+pouyarezvani@users.noreply.github.com>
2019-12-08 14:47:03 -0800

    updated Post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,7 +9,7 @@ const db = require("../models");
 const createPost = (req, res) => {
   db.Post.create(
     { user: req.session.currentUser.id, ...req.body },
-    (err, foundPost) => {
+    (err, createdPost) => {
       if (err)
         return res.status(500).json({
           status: 500,
@@ -19,7 +19,7 @@ const createPost = (req, res) => {
       res.status(200).json({
         status: 200,
         messsage: "Post Created",
-        data: foundPost
+        data: createdPost
       });
     }
   );
@@ -63,8 +63,31 @@ const findPost = (req, res) => {
 
 // Edit a Post
 
-const findPost = (req, res) => {
-  db.Post.findById(req.params.post_id, (err, foundPost) => {
+const editPost = (req, res) => {
+  db.Post.findByIdAndUpdate(
+    req.params.post_id,
+    req.body,
+    { new: true },
+    (err, updatedPost) => {
+      if (err)
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong, please try again"
+        });
+
+      res.status(200).json({
+        status: 200,
+        messsage: "Post successfully updated.",
+        data: updatedPost
+      });
+    }
+  );
+};
+
+// Delete a Post
+
+const deletePost = (req, res) => {
+  db.Post.findByIdAndDelete(req.params.post_id, (err, deletedPost) => {
     if (err)
       return res.status(500).json({
         status: 500,
@@ -73,18 +96,16 @@ const findPost = (req, res) => {
 
     res.status(200).json({
       status: 200,
-      messsage: "Post successfully found.",
-      data: foundPost
+      messsage: "Post successfully deleted.",
+      data: deletedPost
     });
   });
 };
-
-// Delete a Post
 
 module.exports = {
   indexPosts,
   findPost,
   createPost,
-  EditPost,
-  DeletePost
+  editPost,
+  deletePost
 };
