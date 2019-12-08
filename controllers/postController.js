4dd92fe239db6aabf5e68c374e731ca130fe9fdb
pouyarezvani <32917154+pouyarezvani@users.noreply.github.com>
2019-12-08 14:57:13 -0800

    Fixes Post Controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,22 +7,34 @@ const db = require("../models");
 // Create a Post
 
 const createPost = (req, res) => {
-  db.Post.create(
-    { user: req.session.currentUser.id, ...req.body },
-    (err, createdPost) => {
+  const post = {
+    user: req.session.currentUser._id,
+    image: req.body.image,
+    post_text: req.body.post_text
+  };
+
+  db.Post.create(post, (err, createdPost) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong, please try again."
+      });
+
+    db.User.findById(req.session.currentUser._id, (err, foundUser) => {
       if (err)
         return res.status(500).json({
           status: 500,
-          message: "Something went wrong, please try again"
+          message: "Something went wrong, please try again."
         });
+      foundUser.posts.push(createdPost._id);
+      foundUser.save();
+    });
 
-      res.status(200).json({
-        status: 200,
-        messsage: "Post Created",
-        data: createdPost
-      });
-    }
-  );
+    res.status(201).json({
+      status: 201,
+      message: "post successfully created."
+    });
+  });
 };
 
 // Index Posts
@@ -46,7 +58,7 @@ const indexPosts = (req, res) => {
 // Find a Post
 
 const findPost = (req, res) => {
-  db.Post.findById(req.params.post_id, (err, foundPost) => {
+  db.Post.findById(req.params.id, (err, foundPost) => {
     if (err)
       return res.status(500).json({
         status: 500,
@@ -64,24 +76,19 @@ const findPost = (req, res) => {
 // Edit a Post
 
 const editPost = (req, res) => {
-  db.Post.findByIdAndUpdate(
-    req.params.post_id,
-    req.body,
-    { new: true },
-    (err, updatedPost) => {
-      if (err)
-        return res.status(500).json({
-          status: 500,
-          message: "Something went wrong, please try again"
-        });
-
-      res.status(200).json({
-        status: 200,
-        messsage: "Post successfully updated.",
-        data: updatedPost
+  db.Post.findByIdAndUpdate(req.params.id, req.body, (err, updatedPost) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong, please try again"
       });
-    }
-  );
+
+    res.status(200).json({
+      status: 200,
+      messsage: "Post successfully updated.",
+      data: updatedPost
+    });
+  });
 };
 
 // Delete a Post
