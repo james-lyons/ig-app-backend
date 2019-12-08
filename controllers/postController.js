ad8e8b411b3ff5c1a8247c6daf47e14b653c1ff1
pouyarezvani <32917154+pouyarezvani@users.noreply.github.com>
2019-12-08 14:38:27 -0800

    adds Post Controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,90 @@
 // SECTION Modules
 
-const db = require('../models');
+const db = require("../models");
 
 // SECTION Controllers
+
+// Create a Post
+
+const createPost = (req, res) => {
+  db.Post.create(
+    { user: req.session.currentUser.id, ...req.body },
+    (err, foundPost) => {
+      if (err)
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong, please try again"
+        });
+
+      res.status(200).json({
+        status: 200,
+        messsage: "Post Created",
+        data: foundPost
+      });
+    }
+  );
+};
+
+// Index Posts
+
+const indexPosts = (req, res) => {
+  db.Post.find({}, (err, foundPosts) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong, please try again"
+      });
+
+    res.status(200).json({
+      status: 200,
+      messsage: "Posts successfully found.",
+      data: foundPosts
+    });
+  });
+};
+
+// Find a Post
+
+const findPost = (req, res) => {
+  db.Post.findById(req.params.post_id, (err, foundPost) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong, please try again"
+      });
+
+    res.status(200).json({
+      status: 200,
+      messsage: "Post successfully found.",
+      data: foundPost
+    });
+  });
+};
+
+// Edit a Post
+
+const findPost = (req, res) => {
+  db.Post.findById(req.params.post_id, (err, foundPost) => {
+    if (err)
+      return res.status(500).json({
+        status: 500,
+        message: "Something went wrong, please try again"
+      });
+
+    res.status(200).json({
+      status: 200,
+      messsage: "Post successfully found.",
+      data: foundPost
+    });
+  });
+};
+
+// Delete a Post
+
+module.exports = {
+  indexPosts,
+  findPost,
+  createPost,
+  EditPost,
+  DeletePost
+};
